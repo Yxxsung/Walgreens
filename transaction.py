7737a2bffc51a1d075b3e04e877cf395be185405
Sophia Alexander <32229134+Yxxsung@users.noreply.github.com>
2024-04-30 20:26:56 -0400

    Transactions file start

---
+++ transaction.py
@@ -0,0 +1,13 @@
+#This file is meant to be the code for all forms of transactions
+#Transaction, Checkout, Return, and void
+
+import inventory
+
+
+#Transaction Function meant to collect items for checkout
+def Transaction():
+    
+
+
+#Checkout function
+def Checkout():
